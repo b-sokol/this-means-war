03068228f14c7d87c0e53ae3d1ed69891261f22a
sokolb <sokolb@mac.com>
2020-08-07 20:08:58 -0400

    add wireframe images, README.md, CSS for cards, JS section outlines, and basic HTML outline

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*------------- constants -------------*/
+/*------ app's state (variables) ------*/
+/*----- cached element references -----*/
+/*---------- event listeners ----------*/
+/*------------- functions -------------*/
+function initialize(e) {
+
+};
+
+function render() {
+
+};
+
+render();
+
+initialize();
