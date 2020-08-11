2f04e361cbb67f3a0c44a996cacc922aa5c04a92
sokolb <sokolb@mac.com>
2020-08-11 14:28:04 -0400

    add gameboard setup to HTML, add constants, state, cached elements, and event listeners to js, stub up functions in js, clear CSS and do not display deploy and redeploy buttons on load in CSS

--- js/main.js
+++ js/main.js
@@ -1,13 +1,46 @@
 /*------------- constants -------------*/
+
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const masterDeck = buildMasterDeck();
+
 /*------ app's state (variables) ------*/
+
+let suffledDeck;
+let deckOne;
+let deckTwo;
+let spoils;
+
 /*----- cached element references -----*/
+
+const playerOneDeck = document.getElementById('player-one-deck');
+const playerTwoDeck = document.getElementById('player-two-deck');
+const battlefieldDeck = document.getElementById('battleground-deck');
+const spoilsDeck = document.getElementById('spoils');
+const deploy = document.getElementById('deploy');
+const redeploy = document.getElementById('redeploy');
+
 /*---------- event listeners ----------*/
+
+document.getElementById('play').addEventListener('click', play);
+deploy.addEventListener('click', playCard);
+redeploy.addEventListener('click', playCard);
+
 /*------------- functions -------------*/
+
 function initialize(e) {
 
 };
 
 function render() {
+
+};
+
+function play() {
+
+};
+
+function playCard() {
 
 };
 
