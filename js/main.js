a9883ceb9dd81bd3c31e8389a86b763f8b1fbd4b
sokolb <sokolb@mac.com>
2020-08-11 22:33:08 -0400

    change css/html from table to grid, add to initialize function, complete renderDeckDown function, add to play function, rearrange card values so Aces come first, add players constant, change and add to cached element references fornaming conventions, add event listeners to submit player names

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 /*------------- constants -------------*/
 
-const suits = ["s", "c", "d", "h"];
+const suits = ['s', 'c', 'd', 'h'];
 const ranks = [
-  "02",
-  "03",
-  "04",
-  "05",
-  "06",
-  "07",
-  "08",
-  "09",
-  "10",
-  "J",
-  "Q",
-  "K",
-  "A",
+  'A',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
+  '10',
+  'J',
+  'Q',
+  'K',
 ];
 const masterDeck = buildMasterDeck();
+
+const players = {
+  '1': {
+    name: 'Player One',
+    deck: [],
+    deckEl: null,
+    isWinner: false,
+  },
+  '-1': {
+    name: 'Player Two',
+    deck: [],
+    deckEl: null,
+    isWinner: false,
+  },
+};
 
 /*------ app's state (variables) ------*/
 
@@ -27,22 +42,42 @@ let spoils = [];
 
 /*----- cached element references -----*/
 
-const playerOneDeck = document.getElementById("player-one-deck");
-const playerTwoDeck = document.getElementById("player-two-deck");
-const battlefieldDeck = document.getElementById("battleground-deck");
-const spoilsDeck = document.getElementById("spoils");
-const deploy = document.getElementById("deploy");
-const redeploy = document.getElementById("redeploy");
+const deckOneEl = document.getElementById('player-one-deck');
+const deckTwoEl = document.getElementById('player-two-deck');
+const battlefieldDeckEl = document.getElementById('battleground-deck');
+const spoilsEl = document.getElementById('spoils');
+const deployEl = document.getElementById('deploy');
+const redeployEl = document.getElementById('redeploy');
+const playEl = document.getElementById('play');
+const playerOneNameEl = document.getElementById('player-one-name');
+const playerTwoNameEl = document.getElementById('player-two-name');
+const playerOneNameSubmitEl = document.getElementById('player-one-name-submit');
+const playerTwoNameSubmitEl = document.getElementById('player-two-name-submit');
 
 /*---------- event listeners ----------*/
 
-document.getElementById("play").addEventListener("click", play);
-deploy.addEventListener("click", playCard);
-redeploy.addEventListener("click", playCard);
+playEl.addEventListener('click', play);
+deployEl.addEventListener('click', playCard);
+redeployEl.addEventListener('click', initialize);
+playerOneNameSubmitEl.addEventListener('click', () => {
+  if (playerOneNameEl.value !== '') {
+    players[1].name = playerOneNameEl.value;
+  }
+});
+playerTwoNameSubmitEl.addEventListener('click', () => {
+  if (playerTwoNameEl.value !== '') {
+    players[-1].name = playerTwoNameEl.value;
+  }
+});
 
 /*------------- functions -------------*/
 
-function initialize(e) {}
+function initialize() {
+  deployEl.style.display = 'none';
+  redeployEl.style.display = 'none';
+  renderDeckInContainer(masterDeck, battlefieldDeckEl);
+
+}
 
 function render() {}
 
@@ -60,7 +95,7 @@ function buildMasterDeck() {
 
 function shuffleDeck(deck, newDeck) {
   const tempDeck = [...deck];
-  const newShuffledDeck = [];
+  //   const newShuffledDeck = [];
   while (tempDeck.length) {
     const rndIdx = Math.floor(Math.random() * tempDeck.length);
     newDeck.push(tempDeck.splice(rndIdx, 1)[0]);
@@ -73,31 +108,82 @@ function getCardValue(c) {
   let card = '';
   for (let j = 1; j < cardFace.length; j++) {
     card += cardFace.charAt(j);
-  };
+  }
   switch (card) {
-    case "J":
+    case 'J':
       cardValue = 11;
       break;
-    case "Q":
+    case 'Q':
       cardValue = 12;
       break;
-    case "K":
+    case 'K':
       cardValue = 13;
       break;
-    case "A":
+    case 'A':
       cardValue = 14;
       break;
     default:
       cardValue = parseInt(card);
-  };
+  }
   return cardValue;
 }
 
-function dealDecks() {}
+function deal(dShuffled, dOne, dTwo) {
+  for (let i = 0; i < dShuffled.length; i++) {
+    if (i % 2 === 1) {
+      dOne.push(dShuffled[i]);
+    } else {
+      dTwo.push(dShuffled[i]);
+    }
+  }
+}
 
-function play() {}
+function renderDeckInContainer(deck, container) {
+  container.innerHTML = '';
+  const cardsHtml = deck.reduce((html, card) => {
+    let index = deck.indexOf(card);
+    return html + `<div class="card ${card.face}" style="z-index: ${index}; position: absolute; left: ${
+      index * 15 + 'px'
+    }"></div>`;
+  }, '');
+  container.innerHTML = cardsHtml;
+}
 
-function playCard() {}
+function renderDeckDown(player, container) {
+  let deck = players[player].deck;
+  let pName = players[player].name + ' ';
+  for (let i = 0; i < deck.length; i++) {
+    container.innerHTML = '';
+    const cardsBackHtml = deck.reduce((html, card) => {
+      let index = deck.indexOf(card);
+      return (
+        html +
+        `<div class="card back" id="${
+          pName + ' ' + index
+        }" style="z-index: ${index}; position: absolute; left: ${
+          index - 10 + 'px'
+        }"></div>`
+      );
+    }, '');
+    container.innerHTML = cardsBackHtml;
+  }
+}
+
+function play() {
+  battlefieldDeckEl.innerHTML = '';
+  players[1].deck = deckOne;
+  players[1].deckEl = deckOneEl;
+  players[-1].deck = deckTwo;
+  players[-1].deckEl = deckTwoEl;
+  shuffleDeck(masterDeck, shuffledDeck);
+  deal(shuffledDeck, deckOne, deckTwo);
+  playEl.style.display = 'none';
+  deployEl.style.display = 'block';
+  renderDeckDown('1', deckOneEl);
+  renderDeckDown('-1', deckTwoEl);
+}
+
+function playCard(e) {}
 
 render();
 
